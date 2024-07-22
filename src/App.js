13794786e558519af65939335db58e38f4494040
minhdang0907 <122429385+minhdang0907@users.noreply.github.com>
2024-07-22 10:30:44 +0700

    Minhdang (#2)
    * First commit
    
    * Fix UI User

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import  Detail  from './Pages/Detail';
+import Detail from './Pages/Detail';
 import HomePage from './Pages/HomePage';
 import Carts from './Pages/Carts';
 import SignupPage from './Pages/SignupPage';
 import LoginPage from './Pages/LoginPage';
+import { ProductList } from './Components/ProductList';
+
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path='/product/:id' element={<Detail/>}/>
-      <Route path='/cart' element={<Carts/>}/>
-      <Route path="/login" element={<LoginPage />} /> {/* New route for Login */}
-      <Route path='/signup' element={<SignupPage/>}/>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/category/:categoryid" element={<HomePage />} />
+          <Route path="/product/:id" element={<Detail />} />
+          <Route path="/login" element={<LoginPage />} /> {/* New route for Login */}
+          <Route path='/signup' element={<SignupPage/>}/>
+          <Route path="/cart" element={<Carts />} />
+        </Routes>
+      </BrowserRouter>
+
     </>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,57 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './Header.css';
+
 export default function Header() {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:9999/categories').then((response) => {
+            setCategories(response.data);
+        });
+    }, []);
+
     return (
         <div>
-             <div className="header">
-        <Container>
-          <div className="header-content">
-            Ưu đãi 5% cho đơn hàng nguyên giá đầu tiên* | Nhập mã: MLBWELCOME
-          </div>
-        </Container>
-      </div>
+            <div className="header">
+                <Container>
+                    <div className="header-content">
+                        Ưu đãi 5% cho đơn hàng nguyên giá đầu tiên* | Nhập mã: MLBWELCOME
+                    </div>
+                </Container>
+            </div>
 
-      {/* Navbar */}
-      <Navbar bg="light" expand="lg" className="main-navbar">
-        <Container>
-          <Navbar.Brand href="/">
-            <img
-              src="https://1000logos.net/wp-content/uploads/2017/04/MLB-Logo.png"  
-              alt="Logo"
-              className="d-inline-block align-top"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">QUẦN ÁO</Nav.Link>
-              <Nav.Link href="#link">MŨ NÓN</Nav.Link>
-              <Nav.Link href="#link">GIÀY DÉP</Nav.Link>
-              <Nav.Link href="#link">TÚI VÍ</Nav.Link>
-              <Nav.Link href="#link">PHỤ KIỆN</Nav.Link>
-            </Nav>
-            <Nav className="ml-auto">
-              <Nav.Link href="#search">
-                <i className="bi bi-search"></i> 
-              </Nav.Link>
-              <Nav.Link href="/cart">
-                <i className="bi bi-bag"></i>
-              </Nav.Link>
-              <Nav.Link href="#wishlist">
-                <i className="bi bi-heart"></i>
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <i className="bi bi-person"></i>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-   
+
+            <Navbar bg="light" expand="lg" className="main-navbar">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img
+                            src="https://1000logos.net/wp-content/uploads/2017/04/MLB-Logo.png"
+                            alt="Logo"
+                            className="d-inline-block align-top"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            {categories.map((category) => (
+                                <LinkContainer key={category.id} to={`/category/${category.id}`}>
+                                    <Nav.Link>{category.name}</Nav.Link>
+                                </LinkContainer>
+                            ))}
+                        </Nav>
+                        <Nav className="ml-auto">
+                            <Nav.Link href="#search">
+                                <i className="bi bi-search"></i>
+                            </Nav.Link>
+                            <LinkContainer to="/cart">
+                                <Nav.Link>
+                                    <i className="bi bi-bag"></i>
+                                </Nav.Link>
+                            </LinkContainer>
+                            <Nav.Link href="#wishlist">
+                                <i className="bi bi-heart"></i>
+                            </Nav.Link>
+                            <Nav.Link href="/login">
+                                <i className="bi bi-person"></i>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
         </div>
-        
     );
 }
