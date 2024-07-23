fd4f95630e74294e9f1d9805299d248bf146501e
minhdang0907 <122429385+minhdang0907@users.noreply.github.com>
2024-07-24 02:37:52 +0700

    Minhdang (#5)
    * Merge main
    
    * VerifyOrder feature

---
+++ src/Pages/VerifyOrder.js
@@ -0,0 +1,12 @@
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import CheckOut from "../Components/Checkout";
+export default function VerifyOrder() {
+    return (
+        <div>
+            <Header />
+            <CheckOut />
+            <Footer/>
+        </div>
+    );
+}
