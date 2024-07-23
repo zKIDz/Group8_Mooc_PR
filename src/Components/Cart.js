0daf3332199778653abf29b9ad37fec85b9fc5e9
minhdang0907 <122429385+minhdang0907@users.noreply.github.com>
2024-07-24 02:37:01 +0700

    VerifyOrder feature

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,18 +1,23 @@
 import { Container, Row, Col, Table, Button, Form } from "react-bootstrap";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Cart() {
+    const navigate = useNavigate();
     const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem('cart')) || []);
-    
+
+    const handleCheckout = () => {
+        if (cartItems.length > 0) {
+            navigate('/verify-order', { state: { cartItems } });
+        }
+    };
+
     useEffect(() => {
         localStorage.setItem('cart', JSON.stringify(cartItems));
     }, [cartItems]);
 
     const subTotal = cartItems.reduce((acc, item) => acc + (item.price * item.quantity), 0);
-    const shippingCost = subTotal ? 5 : 0; 
-    const total = subTotal + shippingCost;
+    
 
     const handleClearCart = () => {
         localStorage.removeItem('cart');
@@ -20,6 +25,7 @@ export default function Cart() {
         window.location.reload(); 
         setCartItems([]);
     };
+
     const handleQuantityChange = (productId, size, quantity) => {
         if (quantity < 1) {
             quantity = 1;
@@ -30,75 +36,68 @@ export default function Cart() {
         setCartItems(updatedCartItems);
     };
 
-    
-
     return (
-        <>
-            <Container style={{ minHeight: '60vh' }}>
-                <h1 className="text-center">SHOPPING CART</h1>
-                <Row className="my-3">
-                    <Col xs={12} md={8}>
-                        {cartItems.length > 0 ? (
-                            <>
-                                <div className="d-flex justify-content-between mb-3">
-                                    <Button variant="danger" onClick={handleClearCart}>Xóa</Button>
-                                </div>
-                                <Table striped bordered hover>
-                                    <thead>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Tên sản phẩm</th>
-                                            <th>Size</th>
-                                            <th>Giá</th>
-                                            <th>Ảnh</th>
-                                            <th>Số lượng</th>
-                                            <th>Tổng</th>
-                                            
+        <Container style={{ minHeight: '60vh' }}>
+            <h1 className="text-center">SHOPPING CART</h1>
+            <Row className="my-3">
+                <Col xs={12} md={8}>
+                    {cartItems.length > 0 ? (
+                        <>
+                            <div className="d-flex justify-content-between mb-3">
+                                <Button variant="danger" onClick={handleClearCart}>Xóa</Button>
+                            </div>
+                            <Table striped bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Tên sản phẩm</th>
+                                        <th>Size</th>
+                                        <th>Giá</th>
+                                        <th>Ảnh</th>
+                                        <th>Số lượng</th>
+                                        <th>Tổng</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {cartItems.map((item) => (
+                                        <tr key={`${item.id}-${item.size}`}>
+                                            <td>{item.id}</td>
+                                            <td>{item.name}</td>
+                                            <td>{item.size}</td>
+                                            <td>${item.price}</td>
+                                            <td><img src={`${item.images[0]?.name}`} alt={item.name} style={{ width: '50px' }} /></td>
+                                            <td>
+                                                <Form.Control 
+                                                    type="number" 
+                                                    value={item.quantity} 
+                                                    min="1"
+                                                    onChange={(e) => handleQuantityChange(item.id, item.size, e.target.value)}
+                                                />
+                                            </td>
+                                            <td>${(item.price * item.quantity).toFixed(2)}</td>
                                         </tr>
-                                    </thead>
-                                    <tbody>
-                                        {cartItems.map((item) => (
-                                            <tr key={`${item.id}-${item.size}`}>
-                                                <td>{item.id}</td>
-                                                <td>{item.name}</td>
-                                                <td>{item.size}</td>
-                                                <td>${item.price}</td>
-                                                <td><img src={`${item.images[0]?.name}`} alt={item.name} style={{ width: '50px' }} /></td>
-                                                <td>
-                                                    <Form.Control 
-                                                        type="number" 
-                                                        value={item.quantity} 
-                                                        min="1"
-                                                        onChange={(e) => handleQuantityChange(item.id, item.size, e.target.value)}
-                                                    />
-                                                </td>
-                                                <td>${(item.price * item.quantity).toFixed(2)}</td>
-                                                
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </Table>
-                            </>
-                        ) : (
-                            <div className="text-center empty-cart">
-                                <p>Không có sản phẩm nào trong giỏ hàng</p>
-                                <Link to="/" className="btn btn-primary">Tiếp tục mua hàng</Link>
-                            </div>
-                        )}
-                    </Col>
-                    <Col xs={12} md={4}>
-                        <div className="order-summary">
-                            <h4>THÔNG TIN ĐƠN HÀNG</h4>
-                            <div className="order-summary-details">
-                                <p>Tạm tính: {subTotal.toFixed(2)} USD</p>
-                                <p>Phí vận chuyển: {shippingCost.toFixed(2)} USD</p>
-                                <h4>Tổng đơn hàng: {total.toFixed(2)} USD</h4>
-                            </div>
-                            <Button className="btn-block" disabled={cartItems.length === 0}>THANH TOÁN</Button>
+                                    ))}
+                                </tbody>
+                            </Table>
+                        </>
+                    ) : (
+                        <div className="text-center empty-cart">
+                            <p>Không có sản phẩm nào trong giỏ hàng</p>
+                            <Link to="/" className="btn btn-primary">Tiếp tục mua hàng</Link>
                         </div>
-                    </Col>
-                </Row>
-            </Container>
-        </>
+                    )}
+                </Col>
+                <Col xs={12} md={4}>
+                    <div className="order-summary">
+                        <h4>THÔNG TIN ĐƠN HÀNG</h4>
+                        <div className="order-summary-details">
+                            
+                            <h4>Tổng đơn hàng: {subTotal.toFixed(2)} USD</h4>
+                        </div>
+                        <Button className="btn-block" disabled={cartItems.length === 0} onClick={handleCheckout}>THANH TOÁN</Button>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 }
