c72bc8ad32e94b90ca7a576327bafb9ab9b31cf0
Luong Huy <Nguyenluonghuyxg@gmail.com>
2024-07-24 09:02:20 +0700

    fix css login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -16,10 +16,10 @@ const Login = () => {
 
     try {
       const response = await axios.get(`http://localhost:9999/users?email=${email}&password=${password}`);
-      
+
       if (response.data.length > 0) {
         const user = response.data[0];
-        localStorage.setItem('token', 'fake-jwt-token'); 
+        localStorage.setItem('token', 'fake-jwt-token');
         localStorage.setItem('role', user.role);
         login(user.role);
 
@@ -37,30 +37,51 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+    <div className="body">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="inner-wrap col">
+            <h2>Login</h2>
+            <form
+              id="form"
+              className="form"
+              onSubmit={handleLogin}>
+              <div  className="form-group" >
+                <label>Email:</label>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div  className="form-group">
+                <label>Password:</label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              {error && <p style={{ color: 'red' }}>{error}</p>}
+              <div className="form-group">
+                <button className="btn form-control btn-success" type="submit">
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        <button type="submit">Login</button>
-      </form>
+      </div>
     </div>
   );
 };
